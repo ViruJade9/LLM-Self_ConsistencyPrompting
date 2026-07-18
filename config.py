431e1c4b0c1fa43a4a9094809bd5d31a796956d6
ViruJade9 <viraljade786@gmail.com>
2026-07-18 22:29:39 +0530

    Apply .gitignore

--- config.py
+++
@@ -1,5 +0,0 @@
-MAX_COUNT = 10
-
-GENERATOR_MODEL = "llama-3.1-8b-instant"
-
-SUMMARY_MODEL = "llama-3.3-70b-versatile"
--- models/groq_client.py
+++
@@ -1,9 +0,0 @@
-from groq import Groq
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-
-client = Groq(
-    api_key = getenv("GROQ_API")
-)
